0b0421c4c922c6e91779d74a7d89c643aa5c78f1
Ellery Queen <yhmsc007@hotmail.com>
2016-03-15 13:18:37 +0800

    chessboard first git

---
+++ src/com/kururu/ChessCoverProblem.java
@@ -0,0 +1,23 @@
+package com.kururu;
+
+public class ChessCoverProblem {
+
+    int gowCount;
+    int Matrix[][] = new int[100][100];
+
+    void chessBoard(int topLeftCornerRow,int topLeftCornerCol,int specialRow,int specialCol,int size){
+        int temporarySize;//每一次的迭代的棋盘大小
+        int temporaryGowCount;//每次覆盖所用的骨牌编号
+        //棋盘只有一个方格的话那必然只有一个特殊方格
+        if(size == 1)
+            return;
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
