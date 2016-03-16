ff02443a5a34d6d53555b6cecc09abf1359ffdce
Ellery Queen <429889664@qq.com>
2016-03-16 10:45:31 +0800

    KMP final git

--- src/com/kururu/dp/KMP.java
+++ src/com/kururu/dp/KMP.java
@@ -5,9 +5,26 @@ package com.kururu.dp;
  */
 public class KMP {
 
-    
+    public static void KMPsearch(String origin, String target, int[] next){
+        int j = 0;
+        for(int i = 0;i < origin.length();i++){
+            while(j > 0 && origin.charAt(i) != target.charAt(j))
+                j = next[j];
+            if(origin.charAt(i) == target.charAt(j))
+                j++;
+            if(j == target.length()){
+                    System.out.println("find at position " + (i-j));
+                    System.out.println(origin.substring(i-j+1,i+1));
+                    j = next[j];
+            }
+
+        }
+    }
 
     public static void main(String[] args) {
-
+        int[] next = new int[100];
+        String origin = "jnfkaehbfgniouqwhbiufgoqhbergiu";
+        String target = "bfgni";
+        KMPsearch(origin,target,next);
     }
 }
